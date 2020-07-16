bcbd18f4314bb4a92809ffa59b482bbab8d4ae2c
Shreya Prasad <shreyapd06@gmail.com>
2020-07-16 14:09:15 +0530

    Update about.js

--- About/about.js
+++ About/about.js
@@ -1,6 +1,6 @@
 const mailgun = require("mailgun-js");
-const DOMAIN = 'https://app.mailgun.com/app/sending/domains/sandboxbf9adbf9c317472b86607b72c4f66501.mailgun.org';
-const api_key=' fc322301f0fccb01b8e0c997413436b6-87c34c41-b55b4a41';
+const DOMAIN = '';
+const api_key='';
 const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 const data = {
 	from: 'Mailgun, me@samples.mailgun.org',
